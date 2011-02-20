f6a7889c133ec1612c09c0740eecd0f262102897
Vlad Patryshev <vpatryshev@gmail.com>
2011-02-19 19:34:57 -0800

    this script runs on the server

---
+++ doxy.sh
@@ -0,0 +1,14 @@
+# Generates images; pass file name without extension
+CACHE=/home/ubuntu/diagrams
+NAME=$1
+TEX=$NAME.tex
+EPS=$NAME.eps
+DVI=$NAME.dvi
+IMG=$NAME.png
+cd $CACHE
+latex $TEX && dvips -E -o $EPS $DVI && epstopdf $EPS && dvipng -T tight -o $IMG $DVI
+rm $EPS $DVI $NAME.log $NAME.aux
+#ln -s $NAME.tex cache/$NAME.tex
+#ln -s $NAME.pdf cache/$NAME.pdf
+#ln -s $NAME.png cache/$NAME.png
+
