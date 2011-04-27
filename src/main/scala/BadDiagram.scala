45d4489542e484de356bda951bacfcaeeeec1745
Vlad Patryshev <vpatryshev@gmail.com>
2011-04-26 21:53:35 -0700

    Got rid of nulls; Fixed an error in img src attribute processing (ampersand was being escaped). Works now, kind of - except for image timouts on samples page. Will work on it.

---
+++ src/main/scala/BadDiagram.scala
@@ -0,0 +1,12 @@
+package
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vlad
+ * Date: Apr 26, 2011
+ * Time: 5:53:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class BadDiagram
